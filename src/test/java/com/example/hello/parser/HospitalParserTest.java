02c00865788f33985e2fc83f61d0eb582d2024e4
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-31 15:31:57 +0900

    [update] ReadLineContext 예외처리

--- src/test/java/com/example/hello/parser/HospitalParserTest.java
+++ src/test/java/com/example/hello/parser/HospitalParserTest.java
@@ -3,14 +3,31 @@ package com.example.hello.parser;
 import com.example.hello.domain.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootApplication
 class HospitalParserTest {
 
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
     String lineFirst = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+
+    @Test
+    void name() throws IOException {
+        String filename = "/Users/jinhyuck/Downloads/전국병의원정보.csv";
+        List<Hospital> hospitals = hospitalReadLineContext.readByLine(filename);
+        System.out.println("hospitals.size = " + hospitals.size());
+//        assertTrue(hospitals.size() > 10000);
+//        assertTrue(hospitals.size() > 100000);
+    }
 
     @Test
     @DisplayName("csv 1줄을 hospital로 잘 만드는지")
@@ -19,8 +36,8 @@ class HospitalParserTest {
         Hospital hospital = hp.parse(lineFirst);
         assertEquals(1, hospital.getId());
         assertEquals("의원", hospital.getOpenServiceName());
-        assertEquals(3620000,hospital.getOpenLocalGovernmentCode());
-        assertEquals("PHMA119993620020041100004",hospital.getManagementNumber());
+        assertEquals(3620000, hospital.getOpenLocalGovernmentCode());
+        assertEquals("PHMA119993620020041100004", hospital.getManagementNumber());
         assertEquals(LocalDateTime.of(1999, 06, 12, 0, 0, 0), hospital.getLicenseDate());
         assertEquals(1, hospital.getBusinessStatus());
         assertEquals(13, hospital.getBusinessStatusCode());
