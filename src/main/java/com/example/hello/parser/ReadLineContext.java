9b2befddbc7eb1dee31a3c881eaaf40d3c85e86d
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-31 16:17:04 +0900

    [update] ReadLineContext 예외처리 수정

--- src/main/java/com/example/hello/parser/ReadLineContext.java
+++ src/main/java/com/example/hello/parser/ReadLineContext.java
@@ -19,8 +19,13 @@ public class ReadLineContext<T> {
         BufferedReader reader = new BufferedReader(new FileReader(filename));
         String str;
         while ((str = reader.readLine()) != null) {
-            result.add(parser.parse(str));
+            try {
+                result.add(parser.parse(str));
+            } catch (Exception e) {
+                System.out.printf("파싱중 문제가 생겨 이 라인은 넘어갑니다. 파일번호:%s\n", str.substring(0, 1));
+            }
         }
+
         reader.close();
         return result;
     }
