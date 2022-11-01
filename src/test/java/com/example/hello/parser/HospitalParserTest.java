5a33fbe3e697a62276639613a60409b9ac63c459
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-01 14:36:50 +0900

    [update] save, deleteAll, findById test 성공

--- src/test/java/com/example/hello/parser/HospitalParserTest.java
+++ src/test/java/com/example/hello/parser/HospitalParserTest.java
@@ -1,6 +1,8 @@
 package com.example.hello.parser;
 
+import com.example.hello.dao.HospitalDao;
 import com.example.hello.domain.Hospital;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,13 @@ class HospitalParserTest {
 
     @Autowired
     ReadLineContext<Hospital> hospitalReadLineContext;
+    @Autowired
+    HospitalDao hospitalDao;
+
+    @BeforeEach
+    void setup() {
+        hospitalDao.deleteAll();
+    }
 
     String lineFirst = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
     @Test
@@ -53,11 +62,12 @@ class HospitalParserTest {
     }
 
     @Test
-    void hospital() {
-        HospitalParser hospitalParser = new HospitalParser();
-        Hospital h1 = hospitalParser.parse(lineFirst);
-        String hi = h1.toString();
-        System.out.println("hi = " + hi);
-
+    @DisplayName("save가 잘 되는지")
+    void save() {
+        HospitalParser hp = new HospitalParser();
+        Hospital h1 = hp.parse(lineFirst);
+        hospitalDao.save(h1);
+        Hospital findHospital = hospitalDao.findById(1);
+        System.out.println(findHospital.getLicenseDate());
     }
 }
