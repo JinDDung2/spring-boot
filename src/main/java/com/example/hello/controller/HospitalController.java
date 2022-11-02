7d55aaddacd2651bd4d011fc52a86a0ededbf05b
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-02 11:51:22 +0900

    [update] return 로직 리팩토링

--- src/main/java/com/example/hello/controller/HospitalController.java
+++ src/main/java/com/example/hello/controller/HospitalController.java
@@ -2,6 +2,7 @@ package com.example.hello.controller;
 
 import com.example.hello.dao.HospitalDao;
 import com.example.hello.domain.Hospital;
+import com.example.hello.domain.dto.HospitalResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +18,9 @@ public class HospitalController {
     private final HospitalDao hospitalDao;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Hospital> findById(@PathVariable int id) {
-        try {
-            Hospital findHospital = hospitalDao.findById(id);
-            return ResponseEntity.ok()
-                    .body(findHospital);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public ResponseEntity<HospitalResponseDto> findById(@PathVariable int id) {
+        HospitalResponseDto dto = new HospitalResponseDto();
+        return ResponseEntity.ok(
+                dto.from(hospitalDao.findById(id)));
     }
 }
