0cfeb00f3be27ec72b501c0ee1f53ca7ca5fcaf2
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-02 11:28:20 +0900

    [add] HospitalController

---
+++ src/main/java/com/example/hello/controller/HospitalController.java
@@ -0,0 +1,29 @@
+package com.example.hello.controller;
+
+import com.example.hello.dao.HospitalDao;
+import com.example.hello.domain.Hospital;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/hospital")
+public class HospitalController {
+
+    private final HospitalDao hospitalDao;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Hospital> add(@PathVariable int id) {
+        try {
+            Hospital findHospital = hospitalDao.findById(id);
+            return ResponseEntity.ok()
+                    .body(findHospital);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
