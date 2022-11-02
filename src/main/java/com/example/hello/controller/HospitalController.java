7e7dec3bb90cedcf8692d61de687546a59f2fd1e
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-02 14:21:26 +0900

    [update] findById 로직 리팩토링

--- src/main/java/com/example/hello/controller/HospitalController.java
+++ src/main/java/com/example/hello/controller/HospitalController.java
@@ -4,11 +4,14 @@ import com.example.hello.dao.HospitalDao;
 import com.example.hello.domain.Hospital;
 import com.example.hello.domain.dto.HospitalResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +21,14 @@ public class HospitalController {
     private final HospitalDao hospitalDao;
 
     @GetMapping("/{id}")
-    public ResponseEntity<HospitalResponseDto> findById(@PathVariable int id) {
-        HospitalResponseDto dto = new HospitalResponseDto();
-        return ResponseEntity.ok(
-                dto.from(hospitalDao.findById(id)));
+    public ResponseEntity<Hospital> findById(@PathVariable int id) {
+        Hospital findHospital = hospitalDao.findById(id);
+        Optional<Hospital> opt = Optional.of(findHospital);
+
+        if( !opt.isEmpty() ) {
+            return ResponseEntity.ok().body(findHospital);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Hospital());
+        }
     }
 }
